84e223db87df96a07005d66d91142ab320d1a229
James Thompson <jamesrth@usc.edu>
2013-05-20 17:43:47 -0700

    Scalarization

--- src/main/scala/Trie.scala
+++ src/main/scala/Trie.scala
@@ -7,24 +7,22 @@ case class TrieNode(val c: Option[Char],
 
 	def hasChildren = !children.forall(_ == None)
 
-	def addWord(word: String) : Unit = word.isEmpty match {
-		case true => ()
-		case false => {
-			val char = word.head - 'a'
-			if(children(char) == None) children(char) = Some(TrieNode(Some(word.head), false, Some(this)))
-			if(word.length > 1) children(char).get.addWord(word.tail) else {
-				children(char) = Some(TrieNode(Some(word.head), true, Some(this)))
-			}
-		}
+	def addWord(word: String) : Unit = 
+		if(word.isEmpty) () else {
+		val char = word.head - 'a'
+		if(children(char) == None) 
+			children(char) = Some(TrieNode(Some(word.head), false, Some(this)))
+		if(word.length > 1) children(char).get.addWord(word.tail) else 
+			children(char) = Some(TrieNode(Some(word.head), true, Some(this)))
 	}
 
 	def getNode(char: Char) : Option[TrieNode] = children(char - 'a')
 
 	def getWords(l : List[String] = List[String]()) : List[String] = (isWord, hasChildren) match {
-			case (true, true) => toString :: children.flatMap(cw => cw).toList.flatMap { (node: TrieNode) => node.getWords(node.toString :: l) }.reverse
-			case (false, true) => children.flatMap(cw => cw).toList.flatMap { (node: TrieNode) => node.getWords(node.toString :: l) }
-			case (true, false) => List(toString)
-			case (false, false) => Nil
+		case (true, true) => toString :: children.flatMap(cw => cw).toList.flatMap { (node: TrieNode) => node.getWords(node.toString :: l) }.reverse
+		case (false, true) => children.flatMap(cw => cw).toList.flatMap { (node: TrieNode) => node.getWords(node.toString :: l) }
+		case (true, false) => List(toString)
+		case (false, false) => Nil
 	}
 
 	override def toString : String = 
